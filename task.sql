6665e1e48aba4cce31fa1a765eaad07e1ac08c64
Samuel Morgan <samuel.edmund.morgan@icloud.com>
2024-12-26 17:12:49 +0200

    Solution

--- task.sql
+++ task.sql
@@ -1,2 +1,5 @@
 # Write your SQL code for the database creation here. Good luck! 
-USE ShopDB; 
+USE ShopDB;
+CREATE INDEX Name ON ShopDB.Products(Name);
+CREATE INDEX Email ON ShopDB.Customers(Email);
+
